e60e97902a27f7cc01c24e107ec549433732d1f5
Weng Lok <wenglok.khoo@aspert.com.my>
2020-03-10 16:25:30 +0800

    Add Filter,Search field and list display in administration module
    -ProjectMaintenance
    -StatusMaintenance
    -DepartmentMaintenance
    -DocumentTypeMaintenance
    -CompanyMaintenance
    -CurrencyMaintenance
    -EmployeeMaintenance
    -PaymentTermMaintenance
    -LocationMaintenance
    -ItemClassesMaintenance
    -ItemGroupsMaintenance
    -WorkflowApprovalRule
    -VendorGroupMaintenance
    -SystemFlagMaintenance
    -EmployeePositionMaintenance
    -BranchMaintenance
    -CountryMaintenance
    -EmployeeBranchMaintenance
    -EmployeeDepartmentMaintenance
    -EmployeeGroupMaintenance
    -EmployeeProjectMaintenance
    -RegionMaintenance
    -TaxMaintenance

--- wf_project/administration/admin.py
+++ wf_project/administration/admin.py
@@ -2,55 +2,76 @@ from django.contrib import admin
 from .models import WorkflowPattern
 from .models import WorkflowInstance
 from .models import ProjectMaintenance
+from .models import ProjectMaintenanceScreen
 from .models import StatusMaintenance
+from .models import StatusMaintenanceScreen
 from .models import DepartmentMaintenance
+from .models import DepartmentMaintenanceScreen
 from .models import DocumentTypeMaintenance
+from .models import DocumentTypeMaintenanceScreen
 from .models import CompanyMaintenance
+from .models import CompanyMaintenanceScreen
 from .models import CurrencyMaintenance
+from .models import CurrencyMaintenanceScreen
 from .models import EmployeeMaintenance
+from .models import EmployeeMaintenanceScreen
 from .models import UserMaintenance
 from .models import PaymentTermMaintenance
+from .models import PaymentTermMaintenanceScreen
 from .models import LocationMaintenance
+from .models import LocationMaintenanceScreen
 from .models import ItemClassesMaintenance
+from .models import ItemClassesMaintenanceScreen
 from .models import ItemGroupsMaintenance
+from .models import ItemGroupsMaintenanceScreen
 from .models import WorkflowApprovalRule
+from .models import WorkflowApprovalRuleScreen
 from .models import VendorGroupMaintenance
+from .models import VendorGroupMaintenanceScreen
 from .models import SystemFlagMaintenance
+from .models import SystemFlagMaintenanceScreen
 from .models import EmployeePositionMaintenance
+from .models import EmployeePositionMaintenanceScreen
 from .models import BranchMaintenance
+from .models import BranchMaintenanceScreen
 from .models import CountryMaintenance
+from .models import CountryMaintenanceScreen
 from .models import EmployeeBranchMaintenance
+from .models import EmployeeBranchMaintenanceScreen
 from .models import EmployeeDepartmentMaintenance
+from .models import EmployeeDepartmentMaintenanceScreen
 from .models import EmployeeGroupMaintenance
+from .models import EmployeeGroupMaintenanceScreen
 from .models import EmployeeProjectMaintenance
+from .models import EmployeeProjectMaintenanceScreen
 from .models import RegionMaintenance
-from .models import CompanyMaintenanceScreen
+from .models import RegionMaintenanceScreen
 from .models import TaxMaintenance
 from .models import TaxMaintenanceScreen
 
 admin.site.register(WorkflowPattern)
 admin.site.register(WorkflowInstance)
-admin.site.register(ProjectMaintenance)
-admin.site.register(StatusMaintenance)
-admin.site.register(DepartmentMaintenance)
-admin.site.register(DocumentTypeMaintenance)
+admin.site.register(ProjectMaintenance,ProjectMaintenanceScreen)
+admin.site.register(StatusMaintenance,StatusMaintenanceScreen)
+admin.site.register(DepartmentMaintenance,DepartmentMaintenanceScreen)
+admin.site.register(DocumentTypeMaintenance,DocumentTypeMaintenanceScreen)
 admin.site.register(CompanyMaintenance,CompanyMaintenanceScreen)
-admin.site.register(CurrencyMaintenance)
-admin.site.register(EmployeeMaintenance)
+admin.site.register(CurrencyMaintenance,CurrencyMaintenanceScreen)
+admin.site.register(EmployeeMaintenance,EmployeeMaintenanceScreen)
 admin.site.register(UserMaintenance)
-admin.site.register(PaymentTermMaintenance)
-admin.site.register(LocationMaintenance)
-admin.site.register(ItemClassesMaintenance)
-admin.site.register(ItemGroupsMaintenance)
-admin.site.register(WorkflowApprovalRule)
-admin.site.register(VendorGroupMaintenance)
-admin.site.register(SystemFlagMaintenance)
-admin.site.register(EmployeePositionMaintenance)
-admin.site.register(BranchMaintenance)
-admin.site.register(CountryMaintenance)
-admin.site.register(EmployeeBranchMaintenance)
-admin.site.register(EmployeeDepartmentMaintenance)
-admin.site.register(EmployeeGroupMaintenance)
-admin.site.register(EmployeeProjectMaintenance)
-admin.site.register(RegionMaintenance)
+admin.site.register(PaymentTermMaintenance,PaymentTermMaintenanceScreen)
+admin.site.register(LocationMaintenance,LocationMaintenanceScreen)
+admin.site.register(ItemClassesMaintenance,ItemClassesMaintenanceScreen)
+admin.site.register(ItemGroupsMaintenance,ItemGroupsMaintenanceScreen)
+admin.site.register(WorkflowApprovalRule,WorkflowApprovalRuleScreen)
+admin.site.register(VendorGroupMaintenance,VendorGroupMaintenanceScreen)
+admin.site.register(SystemFlagMaintenance,SystemFlagMaintenanceScreen)
+admin.site.register(EmployeePositionMaintenance,EmployeePositionMaintenanceScreen)
+admin.site.register(BranchMaintenance,BranchMaintenanceScreen)
+admin.site.register(CountryMaintenance,CountryMaintenanceScreen)
+admin.site.register(EmployeeBranchMaintenance,EmployeeBranchMaintenanceScreen)
+admin.site.register(EmployeeDepartmentMaintenance,EmployeeDepartmentMaintenanceScreen)
+admin.site.register(EmployeeGroupMaintenance,EmployeeGroupMaintenanceScreen)
+admin.site.register(EmployeeProjectMaintenance,EmployeeProjectMaintenanceScreen)
+admin.site.register(RegionMaintenance,RegionMaintenanceScreen)
 admin.site.register(TaxMaintenance,TaxMaintenanceScreen)
--- wf_project/administration/models.py
+++ wf_project/administration/models.py
@@ -13,6 +13,10 @@ class BranchMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.branch_code, self.branch_name)
 
+class BranchMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('branch_code', 'branch_name','is_active' )
+    search_fields = ('branch_code', 'branch_name',)
+
 class CompanyMaintenance(models.Model):
     company_name = models.CharField(max_length=250)
     short_name = models.CharField(max_length=250)        
@@ -32,7 +36,7 @@ class CompanyMaintenance(models.Model):
 class CompanyMaintenanceScreen(admin.ModelAdmin):
     list_display = ('short_name', 'company_name', 'currency','region','is_active')
     list_filter = ('is_active',)
-    search_fields = ('short_name', 'company_name','currency__currency_name',)
+    search_fields = ('short_name', 'company_name','currency__currency_name','currency__currency_code',)
 
 class CountryMaintenance(models.Model):
     country_code = models.CharField(max_length=3)
@@ -49,6 +53,11 @@ class CountryMaintenance(models.Model):
     def __str__(self):
         return self.country_code
 
+class CountryMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('country_code', 'country_name', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('country_code', 'country_name',)
+
 class CurrencyMaintenance(models.Model):
     currency_code = models.CharField(max_length=100)
     currency_name = models.CharField(max_length=250)
@@ -64,6 +73,11 @@ class CurrencyMaintenance(models.Model):
     def __str__(self):
         return self.currency_name
 
+class CurrencyMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('currency_code', 'currency_name', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('currency_code', 'currency_name',)
+
 class DepartmentMaintenance(models.Model):
     department_code = models.CharField(max_length=100)
     department_name = models.CharField(max_length=250)
@@ -71,6 +85,11 @@ class DepartmentMaintenance(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.department_code, self.department_name)
+
+class DepartmentMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('department_code', 'department_name', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('department_code', 'department_name',)
 
 class DocumentTypeMaintenance(models.Model):
     document_type_code = models.CharField(max_length=100)
@@ -84,6 +103,11 @@ class DocumentTypeMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.document_type_code, self.document_type_name)
 
+class DocumentTypeMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('document_type_code', 'document_type_name', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('document_type_code', 'document_type_name',)
+
 class EmployeeBranchMaintenance(models.Model):
     employee = models.ForeignKey('EmployeeMaintenance', default=0, verbose_name="Employee",on_delete=models.CASCADE)
     branch = models.ForeignKey('BranchMaintenance', default=0, verbose_name="Branch",on_delete=models.CASCADE)
@@ -92,6 +116,10 @@ class EmployeeBranchMaintenance(models.Model):
     modified_by = models.CharField(max_length=100)
     modified_timestamp = models.DateField()
 
+class EmployeeBranchMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('employee', 'branch')
+    search_fields = ('employee__employee_name','employee__employee_code' ,'branch__branch_name','branch__branch_code')
+
 class EmployeeDepartmentMaintenance(models.Model):
     employee = models.ForeignKey('EmployeeMaintenance', default=0, verbose_name="Employee",on_delete=models.CASCADE)
     department = models.ForeignKey('DepartmentMaintenance', default=0, verbose_name="Department",on_delete=models.CASCADE)
@@ -99,6 +127,10 @@ class EmployeeDepartmentMaintenance(models.Model):
     created_timestamp = models.DateField()
     modified_by = models.CharField(max_length=100)
     modified_timestamp = models.DateField()
+
+class EmployeeDepartmentMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('employee', 'department')
+    search_fields = ('employee__employee_name','employee__employee_code' ,'department__department_name','department__department_code')
 
 class EmployeeGroupMaintenance(models.Model):
     group_code = models.CharField(max_length=100)
@@ -111,15 +143,19 @@ class EmployeeGroupMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.group_code, self.group_name)
 
+class EmployeeGroupMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('group_code', 'group_name',)
+    search_fields = ('group_code', 'group_name',)
+
 class EmployeeMaintenance(models.Model):
     gender_option = [('M','MALE'),('F','FEMALE')]
-    
     employee_name = models.CharField(max_length=250)
     nick_name = models.CharField(max_length=250)
     gender = models.CharField(max_length=25,choices=gender_option)
     dob = models.DateField(verbose_name="Date of Birth")
     position_id = models.ForeignKey('EmployeePositionMaintenance',default=0,verbose_name="Position",on_delete=models.CASCADE)
     reporting_officer_id = models.ForeignKey('self',default=0,verbose_name="Reporting Officer",on_delete=models.CASCADE)
+    is_active = models.BooleanField()
     created_by = models.CharField(max_length=100,editable=False)
     created_timestamp = models.DateField(editable=False)
     modified_by = models.CharField(max_length=100,editable=False)
@@ -128,6 +164,11 @@ class EmployeeMaintenance(models.Model):
     def __str__(self):
         return self.employee_name
 
+class EmployeeMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('nick_name', 'employee_name','position_id')
+    list_filter = ('is_active',)
+    search_fields = ('nick_name', 'employee_name','position_id__position_name')
+
 class EmployeeProjectMaintenance(models.Model):
     employee = models.ForeignKey('EmployeeMaintenance', default=0, verbose_name="Employee",on_delete=models.CASCADE)
     project = models.ForeignKey('ProjectMaintenance', default=0, verbose_name="Project",on_delete=models.CASCADE)
@@ -135,6 +176,10 @@ class EmployeeProjectMaintenance(models.Model):
     created_timestamp = models.DateField()
     modified_by = models.CharField(max_length=100)
     modified_timestamp = models.DateField()
+
+class EmployeeProjectMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('employee', 'project')
+    search_fields = ('employee__employee_name','employee__employee_code' ,'project__project_code','project__project_name','project__phase_name','project__sub_phase_name')
 
 class EmployeePositionMaintenance(models.Model):
     position_name = models.CharField(max_length=250)
@@ -145,6 +190,10 @@ class EmployeePositionMaintenance(models.Model):
 
     def __str__(self):
         return self.position_name
+
+class EmployeePositionMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('position_name',)
+    search_fields = ('position_name',)
 
 class ItemClassesMaintenance(models.Model):
     item_class_code = models.CharField(max_length=100)
@@ -157,6 +206,11 @@ class ItemClassesMaintenance(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.item_class_code, self.item_class_name)
+
+class ItemClassesMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('item_class_code', 'item_class_name','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('item_class_code', 'item_class_name',)
 
 class ItemGroupsMaintenance(models.Model):
     parent_id = models.ForeignKey('self', default=0, verbose_name="Parent Group", on_delete=models.CASCADE)
@@ -171,6 +225,11 @@ class ItemGroupsMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.item_group_code, self.item_group_name)
 
+class ItemGroupsMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('item_group_code', 'item_group_name','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('item_group_code', 'item_group_name',)
+
 class LocationMaintenance(models.Model):
     loc_code = models.CharField(max_length=100)
     loc_name = models.CharField(max_length=250)
@@ -182,6 +241,10 @@ class LocationMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.loc_code, self.loc_name)
 
+class LocationMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('loc_code', 'loc_name')
+    search_fields = ('loc_code', 'loc_name',)
+
 class PaymentTermMaintenance(models.Model):
     term_code = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
@@ -189,7 +252,12 @@ class PaymentTermMaintenance(models.Model):
     is_active = models.BooleanField()
 
     def __str__(self):
-        return self.term_code
+        return self.description
+
+class PaymentTermMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('term_code', 'description','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('term_code', 'description',)
 
 class ProjectMaintenance(models.Model):
     company = models.ForeignKey('CompanyMaintenance', default=0, verbose_name="Company Name",on_delete=models.CASCADE)
@@ -203,6 +271,10 @@ class ProjectMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.project_code, self.project_name)
 
+class ProjectMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('project_code','company','project_name','phase_name','sub_phase_name')
+    search_fields = ('company__company_name', 'company__company_code','project_code','project_name','phase_name','sub_phase_name',)
+
 class RegionMaintenance(models.Model):
     region_code = models.CharField(max_length=100)
     region_name = models.CharField(max_length=250)
@@ -215,6 +287,11 @@ class RegionMaintenance(models.Model):
     def __str__(self):
         return "%s - %s" % (self.region_code, self.region_name)
 
+class RegionMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('region_code', 'region_name','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('region_code', 'region_name',)
+
 class StatusMaintenance(models.Model):
     document_type = models.ForeignKey('DocumentTypeMaintenance', default=0, verbose_name="Document Type",on_delete=models.CASCADE)
     status_code = models.IntegerField()
@@ -222,6 +299,10 @@ class StatusMaintenance(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.status_code, self.status_name)
+
+class StatusMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('status_code','status_name','document_type')
+    search_fields = ('status_code', 'status_name','document_type')
 
 class UserMaintenance(models.Model):
     company = models.ForeignKey('CompanyMaintenance', default=0, verbose_name="Company Name",on_delete=models.CASCADE)
@@ -240,6 +321,10 @@ class WorkflowApprovalRule(models.Model):
 
     def __str__(self):
         return "Level %s - %s" % (self.ApprovalLevel, self.Description)
+
+class WorkflowApprovalRuleScreen(admin.ModelAdmin):
+    list_display = ('ApprovalLevel', 'Description','Condition')
+    search_fields = ('ApprovalLevel', 'Description','Condition')
 
 class WorkflowInstance(models.Model):
     template_code = models.CharField(max_length=100)
@@ -271,6 +356,11 @@ class VendorGroupMaintenance(models.Model):
     def __str__(self):
         return self.group_name
 
+class VendorGroupMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('group_code', 'group_name','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('group_code', 'group_name')
+
 class SystemFlagMaintenance(models.Model):
     flag_name= models.CharField(max_length=250)
     table_id = models.IntegerField()
@@ -281,6 +371,10 @@ class SystemFlagMaintenance(models.Model):
 
     def __str__(self):
         return self.flag_name
+
+class SystemFlagMaintenanceScreen(admin.ModelAdmin):
+    list_display = ('flag_name', 'table_id')
+    search_fields = ('flag_name', 'table_id')
 
 class TaxMaintenance(models.Model):
     tax_code= models.CharField(max_length=100)
@@ -296,6 +390,6 @@ class TaxMaintenance(models.Model):
         return self.tax_name
 
 class TaxMaintenanceScreen(admin.ModelAdmin):
-    list_display = ('tax_code', 'tax_name', 'rate')
-    list_filter = ('rate',)
+    list_display = ('tax_code', 'tax_name', 'rate','is_active')
+    list_filter = ('is_active',)
     search_fields = ('tax_code', 'tax_name','rate',)

291f8a85f52dae2d304d77a24c756ca39524c0fe
Weng Lok <wenglok.khoo@aspert.com.my>
2020-04-02 14:40:52 +0800

    repush -Add memo logic to be able to add attachment at create

--- wf_project/memo/views.py
+++ wf_project/memo/views.py
@@ -75,9 +75,72 @@ def memo_create(request):
 
             return redirect(memo_list)
     else:
-        memo = Memo
-        form = NewMemoForm()
-    return render(request, 'memo/create.html', {'memo': memo, 'form': form})
+        memo = Memo.objects.create(submit_by=request.user)
+    return redirect(memo_create_edit, memo.pk)
+
+@login_required
+def memo_create_edit(request, pk):    
+    memo = get_object_or_404(Memo, pk=pk)
+    if request.method == 'POST':
+        form = NewMemoForm(request.POST, instance=memo)
+        if form.is_valid():
+
+            memo_type = DocumentTypeMaintenance.objects.filter(document_type_name="Memo")[0]
+            document_number = memo_type.running_number + 1
+            memo_type.running_number = document_number 
+            memo_type.save()
+
+            company = form.cleaned_data['company']
+            department = form.cleaned_data['department']
+            project = form.cleaned_data['project']
+            template = form.cleaned_data['template']
+            memo.document_number = '{0}-{1:05d}'.format(memo_type.document_type_code,document_number)
+            memo.company = company
+            memo.department = department
+            memo.project = project
+            memo.template = template
+            memo.submit_by = request.user
+            memo.save()
+
+            transaction_type = get_object_or_404(TransactiontypeMaintenance,transaction_type_name="BLANK", document_type=memo_type)
+            approval_level = get_object_or_404(WorkflowApprovalRule,approval_level=2)
+
+            approval_item = ApprovalItem()        
+            approval_item.document_number = memo.document_number
+            approval_item.document_pk = memo.pk
+            approval_item.document_type = memo_type
+            approval_item.transaction_type = transaction_type
+            approval_item.approval_level = approval_level
+            approval_item.notification = ""
+            approval_item.status = "D"
+            approval_item.save()
+
+            memo.approval = approval_item
+            memo.save()
+
+            return redirect(memo_list)
+    else:
+        form = NewMemoForm(instance=memo)
+    form_attachment = NewMemoAttachmentForm()
+    return render(request, 'memo/create.html', {'memo': memo, 'form': form, 'form_attachment': form_attachment})
+
+@login_required
+def memo_attachment_create_fromcreate(request, pk):    
+    form = NewMemoAttachmentForm(request.POST, request.FILES)
+    if form.is_valid():
+        memo_attachment = form.save(commit=False)
+        memo = get_object_or_404(Memo, pk=pk)
+        memo_attachment.memo = memo
+        memo_attachment.save()
+    
+    return redirect(memo_create_edit, pk=pk) 
+
+@login_required
+def memo_attachment_delete_fromcreate(request, pk):
+    memoattachment =  get_object_or_404(MemoAttachment, pk=pk)
+    memo = get_object_or_404(Memo, pk=memoattachment.memo.pk)
+    memoattachment.delete()
+    return redirect(memo_create_edit, pk=memo.pk)
 
 @login_required
 def memo_delete(request, pk):

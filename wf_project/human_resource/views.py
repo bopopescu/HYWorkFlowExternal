f831ce3fc1294e06cce93d1b987ac62ee52738a1
Weng Lok <wenglok.khoo@aspert.com.my>
2020-04-14 12:14:15 +0800

    minor update for human resource approval level and do changes on payment forms

--- wf_project/human_resource/views.py
+++ wf_project/human_resource/views.py
@@ -188,7 +188,7 @@ def staff_responsible_delete(request, pk):
 @login_required
 def staff_send_approval(request,pk):
     staff = get_object_or_404(StaffRecruitmentRequest, pk=pk)
-    approval_level = WorkflowApprovalRule.objects.filter(approvel_level=2)[0]
+    approval_level = WorkflowApprovalRule.objects.filter(approval_level=0)[0]
     approval_item = get_object_or_404(ApprovalItem, pk=staff.approval.pk)       
     approval_item.approval_level = approval_level
     if approval_level.ceo_approve == True:

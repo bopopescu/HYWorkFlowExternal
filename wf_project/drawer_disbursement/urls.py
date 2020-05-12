de7186cfa5b8d5fae456b8f80f4de7ed6e09904e
Weng Lok <wenglok.khoo@aspert.com.my>
2020-05-12 18:38:26 +0800

    uncomment the setting back

--- wf_project/drawer_disbursement/urls.py
+++ wf_project/drawer_disbursement/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('drawerdata', views.DrawerViewSet)
-# router.register('disbursementdata', views.DisbursementListViewSet)
-# router.register('disburseddata', views.DisbursedViewSet)
-# router.register('cancelleddata', views.CancelledViewSet)
+router.register('disbursementdata',views.DisbursementListViewSet)
+router.register('disburseddata',views.DisbursedViewSet)
+router.register('cancelleddata',views.CancelledViewSet)
 
 urlpatterns = [ 
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls)), 
     path('drawerselection/', views.drawer_list, name='drawer_selection'),
     path('disbursement_list/<int:drawerpk>/', views.drawer_disbursement_list, name='disbursement_list'),
     path('disbursement_disbursed/<int:pk>&<int:drawerpk>&<str:userid>&<str:password>/', views.drawer_disbursement_disbursed, name='disbursement_disburse'),

57d2bac9dd719d2473c956ad3160defad902d47f
Weng Lok <wenglok.khoo@aspert.com.my>
2020-05-14 16:48:23 +0800

    try to hide for account status

--- wf_project/payment/serializers.py
+++ wf_project/payment/serializers.py
@@ -16,11 +16,11 @@ class PYSerializer(serializers.ModelSerializer):
     submit_by = serializers.StringRelatedField(many=False)
     approval_status = serializers.SerializerMethodField()
     approval_id = serializers.SerializerMethodField()
-    accounts_status = serializers.SerializerMethodField()
+    # accounts_status = serializers.SerializerMethodField()
 
     class Meta:
         model = PaymentRequest
-        fields = ['id', 'revision', 'document_number', 'subject', 'submit_date', 'company', 'project', 'submit_by', 'approval', 'approval_status', 'approval_id', 'utility_account_approval', 'accounts_status']
+        fields = ['id', 'revision', 'document_number', 'subject', 'submit_date', 'company', 'project', 'submit_by', 'approval', 'approval_status', 'approval_id', 'utility_account_approval']
     
     def get_approval_status(self, obj):     
         if obj.approval != None:
@@ -62,21 +62,21 @@ class PYSerializer(serializers.ModelSerializer):
         else:       
             ""
     
-    def get_accounts_status(self, obj):
-        task_exist = AccountTask.objects.filter(approval_item=obj.approval).count()
+    # def get_accounts_status(self, obj):
+    #     task_exist = AccountTask.objects.filter(approval_item=obj.approval).count()
 
-        if task_exist > 0:
-            account_task = get_object_or_404(AccountTask, approval_item=obj.approval)
+    #     if task_exist > 0:
+    #         account_task = get_object_or_404(AccountTask, approval_item=obj.approval)
 
-            if account_task.process:
-                if account_task.completed:
-                    return "Completed"
-                else:
-                    return "In Process"
-            else:
-                return "Pending Process"
-        else:
-            return "Not Applicable"
+    #         if account_task.process:
+    #             if account_task.completed:
+    #                 return "Completed"
+    #             else:
+    #                 return "In Process"
+    #         else:
+    #             return "Pending Process"
+    #     else:
+    #         return "Not Applicable"
 
 class PYItemSerializer(serializers.ModelSerializer):
     tax = serializers.StringRelatedField(many=False)
